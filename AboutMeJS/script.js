18b0b78af26dcc9e24419b0ec7b6aeafe38299b4
Marjorie Preval <122875584+MarsOnTheMoon@users.noreply.github.com>
2023-02-09 16:41:10 +0100

    JS upload.

---
+++ AboutMeJS/script.js
@@ -0,0 +1,34 @@
+<><h2 id="heading">About Marjorie "Mars" Preval</h2>
+
+    <p id="body">
+        I'm from Queens, New York
+        I'm a professional photographer, with a BFA from Parsons: The New School.
+        I take photos, play video games, make candles & home decor, and love sewing & embroidery.
+    </p>
+    
+    <img src="photocollage.jpeg"
+        id="photo"
+        width="1000">
+
+        <p class="inline-box">
+            <li><a class="inline-box" href="https://www.youtube.com/" target="_blank">YouTube</a></li>
+            <li><a class="inline-box" href="https://www.bonappetit.com/recipe/ba-best-lasagna" target="_blank">BonAppetit</a></li>
+            <li><a class="inline-box" href="https://creativemarket.com/" target="_blank">Creative Market</a></li>
+        </p> </>  </>
+
+
+function start() {
+    //get the reference for the header
+    let x = document.getElementById("heading")[1]
+
+    // get the reference for the body
+    let myBody = document.getElementById("body")[1];
+
+    // creates image element
+    let image = document.getElementById("photo")[1]
+
+    // get the links classed as inline-box
+    let y = document.getElementsByClassName("inline-box")
+
+
+}
